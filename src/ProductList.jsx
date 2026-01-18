fad4080cd807ad36d80836f335e5f13d93151702
TaviaKP <taviakpwriteedit@gmail.com>
2026-01-18 00:06:58 -0500

    fixing error

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
